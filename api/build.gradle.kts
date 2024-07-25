9ce31860b4b66b3599c6f202adadbafd42db6fd6
Georgy Steshin <georgys@wix.com>
2024-07-25 22:50:55 +0300

    Removed unused dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -52,10 +52,5 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -52,13 +52,8 @@ afterEvaluate {
 
 dependencies {
     implementation(project(":api"))
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.timber)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
